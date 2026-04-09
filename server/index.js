833ed2f09b9bed188033c2112dd2273217f6f182
Daniel Andreas Wang <codingforpeace@protonmail.com>
2026-04-09 14:31:20 +0200

    set up app instance and middleware

--- server/index.js
+++ server/index.js
@@ -4,3 +4,9 @@ import dotenv from "dotenv";
 import mongoose from "mongoose";
 
 dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(cors());
+app.use(express.json());
