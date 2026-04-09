398bd3c63d3832373b3ea28c1c14c45dddd9205e
Daniel Andreas Wang <codingforpeace@protonmail.com>
2026-04-09 14:41:42 +0200

    set up connection

--- server/index.js
+++ server/index.js
@@ -10,3 +10,16 @@ const PORT = process.env.PORT || 3001;
 
 app.use(cors());
 app.use(express.json());
+
+const start = async() => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URI);
+    console.log("Connected to MongoDB");
+    app.listen(PORT, () => {
+	console.log(`Server running on port ${PORT}`);
+    });
+  } catch (err) {
+      console.error("Connection error:', err);
+      process.exit(1);
+  }
+}
