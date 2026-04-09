9cade559c518ed110c5c9a832769c5ac698653f8
Daniel Andreas Wang <codingforpeace@protonmail.com>
2026-04-09 14:53:21 +0200

    add habit model

---
+++ server/models/Habit.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+const habitSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+}];
+
+export default mongoose.model('Habit', habitSchema);
