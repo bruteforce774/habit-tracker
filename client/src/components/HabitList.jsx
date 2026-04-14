f52472c3ada617deecd4defb61310334daed1381
Daniel Andreas Wang <codingforpeace@protonmail.com>
2026-04-14 21:07:48 +0200

    add component HabitList

---
+++ client/src/components/HabitList.jsx
@@ -0,0 +1,23 @@
+export default function HabitList({ habits, completions, onDelete, onComplete }) {
+  return (
+    <ul>
+      {habits.map((habit) => {
+        const isCompleted = completions.some(
+          (c) => c.habitId === habit._id
+        );
+
+        return (
+          <li key={habit._id}
+            <input
+              type="checkbox"
+              checked={isCompleted}
+              onChange(() => {isCompleted && onComplete(habit._id)}
+            />
+            <span>[habit.name]</span>
+            <button onClick={() => onDelete(habit._id)}>Delete</button>
+          </li>
+        );
+      })}
+    </ul>
+  );
+}
