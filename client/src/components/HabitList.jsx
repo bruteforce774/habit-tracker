13f3f86d695d973ca4311462b1f9af328a1a571f
Daniel Andreas Wang <codingforpeace@protonmail.com>
2026-04-14 21:18:39 +0200

    fix HabitList

--- client/src/components/HabitList.jsx
+++ client/src/components/HabitList.jsx
@@ -5,15 +5,14 @@ export default function HabitList({ habits, completions, onDelete, onComplete })
         const isCompleted = completions.some(
           (c) => c.habitId === habit._id
         );
-
         return (
-          <li key={habit._id}
+          <li key={habit._id}>
             <input
               type="checkbox"
               checked={isCompleted}
-              onChange(() => {isCompleted && onComplete(habit._id)}
+              onChange={() => !isCompleted && onComplete(habit._id)}
             />
-            <span>[habit.name]</span>
+            <span>{habit.name}</span>
             <button onClick={() => onDelete(habit._id)}>Delete</button>
           </li>
         );
