ea2992b12e800999f59b91abd758227bb27bd3eb
Daniel Andreas Wang <codingforpeace@protonmail.com>
2026-04-22 17:15:45 +0200

    change vite config

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,12 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/apps/habit-tracker/',
   plugins: [react()],
-  server: {
-    proxy: {
-      "/api": "http://localhost:3001"
-    }
-  } 
 })
